c2608d45d3db1cb7902914e55c90d3453e23177f
陈导 <cx13062555152@163.com>
2020-06-11 10:40:56 +0800

    更新了

--- run_suite.py
+++ run_suite.py
@@ -22,3 +22,6 @@ with open(report_path, mode='wb') as f:
                                                description="我们的IHRM的接口测试报告")
     # 使用实例化的runner运行测试套件，生成测试报告
     runner.run(suite)
+
+print("--"*100)
+print("增加输出语句,轮训构建")
